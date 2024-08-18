aed73586dadf1a8df199e845358a4929e9ab2163
EC2 Default User <ec2-user@ip-172-31-42-245.eu-west-3.compute.internal>
2024-08-18 17:10:30 +0000

    Applied terraform fmt

--- infrastructure/terraform/modules/edge/main.tf
+++ infrastructure/terraform/modules/edge/main.tf
@@ -14,16 +14,16 @@ resource "aws_wafv2_web_acl" "workshop_web_acl" {
   }
 
   tags = {
-    Name    = join("-", ["WorkshopWebACL", var.suffix])
+    Name = join("-", ["WorkshopWebACL", var.suffix])
   }
 }
 
 resource "terraform_data" "exec_unix" {
   count = lower(var.exec_platform) == "linux" || lower(var.exec_platform) == "macos" ? 1 : 0
   input = join("|", [
-    aws_wafv2_web_acl.workshop_web_acl.name, 
-    aws_wafv2_web_acl.workshop_web_acl.id, 
-    aws_wafv2_web_acl.workshop_web_acl.visibility_config[0].metric_name, 
+    aws_wafv2_web_acl.workshop_web_acl.name,
+    aws_wafv2_web_acl.workshop_web_acl.id,
+    aws_wafv2_web_acl.workshop_web_acl.visibility_config[0].metric_name,
     var.aws_local_profile
   ])
 
@@ -36,7 +36,7 @@ resource "terraform_data" "exec_unix" {
   }
 
   provisioner "local-exec" {
-    command = <<EOT
+    command     = <<EOT
       sh "${path.module}/resources/update-web-acl.sh" "${self.input}"
     EOT
     when        = destroy
@@ -47,22 +47,22 @@ resource "terraform_data" "exec_unix" {
 resource "terraform_data" "exec_windows" {
   count = lower(var.exec_platform) == "windows" ? 1 : 0
   input = join("|", [
-    aws_wafv2_web_acl.workshop_web_acl.name, 
-    aws_wafv2_web_acl.workshop_web_acl.id, 
-    aws_wafv2_web_acl.workshop_web_acl.visibility_config[0].metric_name, 
+    aws_wafv2_web_acl.workshop_web_acl.name,
+    aws_wafv2_web_acl.workshop_web_acl.id,
+    aws_wafv2_web_acl.workshop_web_acl.visibility_config[0].metric_name,
     var.aws_local_profile
   ])
 
   depends_on = [
     aws_wafv2_web_acl.workshop_web_acl
   ]
-  
+
   lifecycle {
     create_before_destroy = true
   }
 
   provisioner "local-exec" {
-    command = <<EOT
+    command     = <<EOT
       PowerShell -ExecutionPolicy Bypass -File "${path.module}/resources/update-web-acl.ps1" -InputParameter "${self.input}"
     EOT
     when        = destroy
