ad34a283f434db5f04552e518843c0ab9eafd316
EC2 Default User <ec2-user@ip-172-31-42-245.eu-west-3.compute.internal>
2024-08-18 16:42:16 +0000

    Replacing hardcoded profile

--- infrastructure/terraform/providers.tf
+++ infrastructure/terraform/providers.tf
@@ -22,5 +22,5 @@ provider "aws" {
   region                   = "us-east-1"
   shared_config_files      = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "terraform-role"
+  profile                  = var.aws_local_profile
 }
